f372b51245d9515de466abc5aaf6c25259b7986f
Milan Stojkovic <ms@getzuper.com>
2020-04-27 23:41:41 +0200

    Test error block

--- app/src/test/java/com/highhopes/myapplication/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/highhopes/myapplication/ui/login/LoginViewModelTest.kt
@@ -22,32 +22,50 @@ class LoginViewModelTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @get:Rule
     val coroutineScope = MainCoroutineScopeRule()
 
     lateinit var viewmodel: LoginViewModel
+
     lateinit var result: Flow<Result<User>>
+    lateinit var error: Flow<Result<User>>
+
     lateinit var userResult: Result<User>
+    lateinit var errorResult: Result<User>
+
     @Mock
     lateinit var user: User
+
     @Mock
     lateinit var useCase: UseCase
+
 
     @Before
     fun setup() {
         viewmodel = LoginViewModel(useCase)
         userResult = Result.Success(user)
         result = flow { emit(userResult) }
+
+        errorResult = Result.Error(Exception(Throwable()))
+        error = flow { emit(errorResult) }
     }
 
     @ExperimentalCoroutinesApi
     @Test
     fun loginOnSuccess() = coroutineScope.runBlockingTest {
-
         Mockito.`when`(useCase.getUsers()).then { result }
         viewmodel.login()
 
         Truth.assertThat(viewmodel.loginResult.value).isEqualTo(userResult)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun loginOnFail() = coroutineScope.runBlockingTest {
+        Mockito.`when`(useCase.getUsers()).then { error }
+        viewmodel.login()
+        Truth.assertThat(viewmodel.loginResult.value).isEqualTo(errorResult)
     }
 
 }
